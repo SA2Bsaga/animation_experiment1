f4530bed4ba0106760553390a9eb2d2d3e5f8740
SA2Bsaga <ramarbro194@gmail.com>
2017-02-05 20:00:59 -0500

    for discussion exploring animation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var yellow = [60, 200, 50];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var color = [yellow, blue, purple];
+
+var myName="Iel Cunanan";
+drawName(myName, color);
+
+bounceBubbles();
+
+bubbleShape="square";
